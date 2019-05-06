490b5bb0feed25cdb840e0d9415e4b65dec34558
Alain Vera <a01272848@itesm.mx>
2019-05-06 08:18:19 -0500

    Sigue habiendo errores, error en la busqueda del glimmer

--- tests/unit/routes/app/actual-juego-test.js
+++ tests/unit/routes/app/actual-juego-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | crear-juego', function(hooks) {
+module('Unit | Route | app/actual-juego', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
